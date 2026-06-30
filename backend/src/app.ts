ff54f22267eedc76533ec6c91b58c19be55d78fe
Pedro Toni Melo Giffoni <pedromelofotografias@gmail.com>
2026-06-30 11:43:06 -0300

    ajuste app ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -28,10 +28,9 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://rede-social-food-snap-git-v3-pedro-giffoni-s-projects.vercel.app",
-    ],
+    origin: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
